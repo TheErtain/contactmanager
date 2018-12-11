55f0122f5cc91580f23036baafd8925dfeaaf9ad
William Watson <vivertain@gmail.com>
2018-12-11 13:07:44 -0800

    Updates

---
+++ precache-manifest.b3e0de7031ebed8519f22846238b290f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "e4a2dbf58f40e3bb1f0a",
+    "url": "/contactmanager/static/js/main.e4a2dbf5.chunk.js"
+  },
+  {
+    "revision": "6c7bf2c4c06721e5b2ca",
+    "url": "/contactmanager/static/js/1.6c7bf2c4.chunk.js"
+  },
+  {
+    "revision": "e4a2dbf58f40e3bb1f0a",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "6c7bf2c4c06721e5b2ca",
+    "url": "/contactmanager/static/css/1.3b303ed8.chunk.css"
+  },
+  {
+    "revision": "f6b685db7797da76922763bd3f250a76",
+    "url": "/contactmanager/index.html"
+  }
+];
